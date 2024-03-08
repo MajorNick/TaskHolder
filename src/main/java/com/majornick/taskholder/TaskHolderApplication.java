56af583aa8c224b0167e7d4d576a2295245fe9c7
MajorNick <nika.chigladze11@gmail.com>
2024-03-08 16:56:40 +0400

    init

---
+++ src/main/java/com/majornick/taskholder/TaskHolderApplication.java
@@ -0,0 +1,13 @@
+package com.majornick.taskholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHolderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskHolderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/majornick/taskholder/TaskHolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majornick.taskholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHolderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
